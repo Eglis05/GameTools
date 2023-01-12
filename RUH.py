8238200d8c27811aca6ff44239283bdf3987a9b3
Eglis Balani <eglisbalani5@gmail.com>
2023-01-12 01:46:17 +0100

    Fixed 10min Error

--- RUH.py
+++ RUH.py
@@ -6,7 +6,7 @@ import undetected_chromedriver as uc
 from urllib.request import urlopen
 from datetime import datetime
 
-clan_id = 167 #ZE: 110, RUH: 113, RG:207, IF: 167
+clan_id = 113 #ZE: 110, RUH: 113, RG:207, IF: 167
 
 one_interval = 30
 values = [1000, 1, 2, 10, 20]
@@ -37,19 +37,19 @@ def get_reps():
     return members
 
 def table(initial_rep, total_rep, changes):
-    sorted_names = list(dict(sorted(changes[names[4]].items(), key=lambda item: item[1], reverse=True)).keys())
+    sorted_names = list(dict(sorted(changes[names[0]].items(), key=lambda item: item[1], reverse=True)).keys())
     if sys.platform.startswith('win'):
         os.system('cls')
     else:
         os.system('clear')
-    print('|{0:3s} | {1:20s} | {2:10s} | {3:8s} | {4:9s} | {5:12s} | {6:10s} | {7:11s} | {8:11s}'.format("Pos", "Clan Name", "initialRep", "totalRep", "totalGain", names[1], names[2], names[3], names[4]))
+    print('|{0:3s} | {1:20s} | {2:10s} | {3:8s} | {4:9s} | {5:12s} | {6:10s} | {7:11s} | {8:11s}'.format("Pos", "Clan Name", "initialRep", "totalRep", "totalGain", names[1], names[2], names[3], names[4]), flush=True)
     line = "-" * 121
-    print(line , sep="", end = "\n")
-    print('|{0:3d} | {1:20s} | {2:10s} | {3:8s} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(0, "Invicta Family (167)", "EGLIS", "GAURAV", sum(changes["total gain"].values()), sum(changes[names[1]].values()), sum(changes[names[2]].values()), sum(changes[names[3]].values()), sum(changes[names[4]].values())))
-    print(line , sep="", end = "\n")
+    print(line , sep="", end = "\n", flush=True)
+    print('|{0:3d} | {1:20s} | {2:10s} | {3:8s} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(0, "RuH Clan (113)", "EGLIS", "GAURAV", sum(changes["total gain"].values()), sum(changes[names[1]].values()), sum(changes[names[2]].values()), sum(changes[names[3]].values()), sum(changes[names[4]].values())), flush=True)
+    print(line , sep="", end = "\n", flush=True)
     for i in range(len(sorted_names)):
         char_name = sorted_names[i]
-        print('|{0:3s} | {1:20s} | {2:10d} | {3:8d} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(f"{i+1}", char_name, initial_rep[char_name], total_rep[char_name], changes["total gain"][char_name], changes[names[1]][char_name], changes[names[2]][char_name], changes[names[3]][char_name], changes[names[4]][char_name]))
+        print('|{0:3s} | {1:20s} | {2:10d} | {3:8d} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(f"{i+1}", char_name, initial_rep[char_name], total_rep[char_name], changes["total gain"][char_name], changes[names[1]][char_name], changes[names[2]][char_name], changes[names[3]][char_name], changes[names[4]][char_name]), flush=True)
     sys.stdout.flush()
 
 def update_burnlist(burnlist, old_burnlist, burntime, changes):
@@ -74,7 +74,7 @@ def check_validity():
     res = urlopen('http://just-the-time.appspot.com/')
     result = res.read().strip().decode('utf-8')
     today_date = datetime.strptime(result, '%Y-%m-%d %H:%M:%S')
-    if today_date >= datetime.strptime("2023-02-01", '%Y-%m-%d'):
+    if today_date >= datetime.strptime("2023-03-15", '%Y-%m-%d'):
         return False
     else:
         return True
