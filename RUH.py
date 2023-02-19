547ec37197340af1f91a21298bce277b8d2da175
Eglis Balani <eglisbalani5@gmail.com>
2023-02-19 16:29:50 +0100

    FB names

--- RUH.py
+++ RUH.py
@@ -5,16 +5,23 @@ import sys
 import undetected_chromedriver as uc
 from urllib.request import urlopen
 from datetime import datetime
+import csv
 
 clan_id = 113 #ZE: 110, RUH: 113, RG:207, IF: 167
 
-one_interval = 30
+one_interval = 5
 values = [1000, 1, 2, 10, 20]
 names = ["total gain", "last 30 secs", "last 1 min"]
 names.extend([f"last {x//2} mins" for x in values[3:]])
 driver = uc.Chrome(use_subprocess=True)
 link = "https://ninjalegends.net/detail_clan.php?clan_id=" + str(clan_id)
 driver.get(link)
+
+dict_members = {}
+with open('members.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        dict_members[row["char_name"]] = row["fb_name"]
 time.sleep(4)
 
 def get_json(link):
@@ -42,14 +49,14 @@ def table(initial_rep, total_rep, changes):
         os.system('cls')
     else:
         os.system('clear')
-    print('|{0:3s} | {1:20s} | {2:10s} | {3:8s} | {4:9s} | {5:12s} | {6:10s} | {7:11s} | {8:11s}'.format("Pos", "Clan Name", "initialRep", "totalRep", "totalGain", names[1], names[2], names[3], names[4]), flush=True)
-    line = "-" * 121
+    print('|{0:3s} | {1:20s} | {2:20s} | {3:10s} | {4:8s} | {5:9s} | {6:12s} | {7:10s} | {8:11s} | {9:11s}'.format("Pos", "Char Name", "FB NAME", "initialRep", "totalRep", "totalGain", names[1], names[2], names[3], names[4]), flush=True)
+    line = "-" * 147
     print(line , sep="", end = "\n", flush=True)
-    print('|{0:3d} | {1:20s} | {2:10s} | {3:8s} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(0, "RuH Clan (113)", "EGLIS", "GAURAV", sum(changes["total gain"].values()), sum(changes[names[1]].values()), sum(changes[names[2]].values()), sum(changes[names[3]].values()), sum(changes[names[4]].values())), flush=True)
+    print('|{0:3d} | {1:20s} | {2:20s} | {3:10s} | {4:8s} | {5:9d} | {6:12d} | {7:10d} | {8:11d} | {9:11d}'.format(0, "RuH Clan (113)", "EGLIS", "SABI", "GAURAV", sum(changes["total gain"].values()), sum(changes[names[1]].values()), sum(changes[names[2]].values()), sum(changes[names[3]].values()), sum(changes[names[4]].values())), flush=True)
     print(line , sep="", end = "\n", flush=True)
     for i in range(len(sorted_names)):
         char_name = sorted_names[i]
-        print('|{0:3s} | {1:20s} | {2:10d} | {3:8d} | {4:9d} | {5:12d} | {6:10d} | {7:11d} | {8:11d}'.format(f"{i+1}", char_name, initial_rep[char_name], total_rep[char_name], changes["total gain"][char_name], changes[names[1]][char_name], changes[names[2]][char_name], changes[names[3]][char_name], changes[names[4]][char_name]), flush=True)
+        print('|{0:3s} | {1:20s} | {2:20s} | {3:10d} | {4:8d} | {5:9d} | {6:12d} | {7:10d} | {8:11d} | {9:11d}'.format(f"{i+1}", char_name, dict_members[char_name], initial_rep[char_name], total_rep[char_name], changes["total gain"][char_name], changes[names[1]][char_name], changes[names[2]][char_name], changes[names[3]][char_name], changes[names[4]][char_name]), flush=True)
     sys.stdout.flush()
 
 def update_burnlist(burnlist, old_burnlist, burntime, changes):
